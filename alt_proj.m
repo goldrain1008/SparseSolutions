f18e851d9990cf6c471fafb27fd7b70cad303f20
goldrain1008 <53974755+goldrain1008@users.noreply.github.com>
2020-12-23 23:11:58 +0900

    Update alt_proj.m

--- alt_proj.m
+++ alt_proj.m
@@ -1,5 +1,6 @@
 %% Exercise 3.4
-function Ealtproj=alt_proj(k,m,n)
+
+function altproj=alt_proj(k,m,n)
     % First part is similar to One Step Sparsification
     xstar=rand([n 1]); 
     xstar(randsample(1:n,n-k),1)=0; 
@@ -7,9 +8,9 @@ function Ealtproj=alt_proj(k,m,n)
     b=A*xstar; 
     x=zeros([n 1]); 
     i=0; % Set initial value of i which would be used in while loop
-    B=pinv(A); 
-    % Calculating pseudoinverse in while loop takes so long, so I  
+    B=pinv(A); % Calculating pseudoinverse in while loop takes so long, so I  
     % just defined it at this point
+    
     while (i<500) % iteration repeats for 500 times
         xbar=x+B*(b-A*x); 
         % first calculate xbar with the initial x, which is an zero vector.
@@ -18,6 +19,6 @@ function Ealtproj=alt_proj(k,m,n)
         i=i+1; % Iterated with altered xbar and x
     end
     xTilde=x; % Substitute xTilde for resultant x
-    Ealtproj=norm(xTilde-xstar)/norm(xstar); % Find error
+    altproj=norm(xTilde-xstar)/norm(xstar); % Find error
 end
         
